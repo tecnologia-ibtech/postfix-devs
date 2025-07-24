a80c78188276cc6510572643008cd139b053c23a
tecnologia-ibtech <31856009+tecnologia-ibtech@users.noreply.github.com>
2025-07-24 16:26:15 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,12 +20,10 @@ echo $RELAY_HOST_NAME > /etc/mailname
 
 # Templates
 j2 /root/conf/postfix-main.cf > /etc/postfix/main.cf
-j2 /root/conf/sender_access > /etc/postfix/sender_access
 j2 /root/conf/recipient_access > /etc/postfix/recipient_access
 j2 /root/conf/sasl_passwd > /etc/postfix/sasl_passwd
 postmap /etc/postfix/sasl_passwd
 postmap /etc/postfix/recipient_access
-postmap /etc/postfix/sender_access
 
 # Custom aliases
 newaliases
